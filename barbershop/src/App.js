e02e195362621f6df47a4041217ebfbc54a487e9
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-09-10 17:48:44 -0600

    Because seperation of concerns is important

--- barbershop/src/App.js
+++ barbershop/src/App.js
@@ -1,29 +1,16 @@
-
 import React, { Component } from 'react';
 import './App.css'
 import PropTypes from 'prop-types';
-import {BrowserRouter, Route, NavLink } from 'react-router-dom'
+import {BrowserRouter, Route } from 'react-router-dom'
 import 'typeface-roboto'
 import { withStyles } from 'material-ui/styles';
-import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
-import IconButton from 'material-ui/IconButton';
+import { CardMedia } from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
-import BusinessIcon from 'material-ui-icons/Business';
-import PhoneIcon from 'material-ui-icons/Phone';
-import DirectionsIcon from 'material-ui-icons/Directions';
-import DateRangeIcon from 'material-ui-icons/DateRange';
-import StarIcon from 'material-ui-icons/Star';
-import SendIcon from 'material-ui-icons/Send';
-import ComputerIcon from 'material-ui-icons/Computer';
-import Divider from 'material-ui/Divider';
-import Drawer from 'material-ui/Drawer';
 import red from 'material-ui/colors/red';
-import Toolbar from 'material-ui/Toolbar';
-import MenuIcon from 'material-ui-icons/Menu';
 import MyTiles from './components/tiles.jsx';
 import MyFooter from './components/footer.jsx';
+import MySideBar from './components/sidebar.jsx';
 
 const styles = theme => ({
     appBar: {
@@ -33,6 +20,10 @@ const styles = theme => ({
         weight: 10,
         backgroundColor: '#090'
     },
+    media: {
+        height: 300,
+        marginBottom:20,
+    },
     paper: theme.mixins.gutters({
         paddingTop: 8,
         paddingBottom: 16,
@@ -41,13 +32,7 @@ const styles = theme => ({
         marginLeft: theme.spacing.unit * 3,
         marginRight: theme.spacing.unit * 3,
     }),
-    card: {
-        maxWidth: 400,
-    },
-    media: {
-        height: 300,
-        marginBottom:20,
-    },
+
     expand: {
         transform: 'rotate(0deg)',
         transition: theme.transitions.create('transform', {
@@ -68,153 +53,40 @@ const styles = theme => ({
         width: '100%',
         backgroundColor:'#24292e',
     },
-    flex: {
-        flex: 1,
-        color: '#FAFAFA'
-    },
-    list: {
-        width: 250,
-        flex: 'initial',
-    },
-    listFull: {
-        width: 'auto',
-        flex: 'initial',
-    },
 });
 
 class App extends Component {
-
-    state = {
-        open: {
-            top: false,
-            left: false,
-            bottom: false,
-            right: false,
-        },
-        expanded: false
-    };
-    toggleDrawer = (side, open) => {
-        const drawerState = {};
-        drawerState[side] = open;
-        this.setState({ open: drawerState });
-    };
-    handleExpandClick = () => {
-        this.setState({ expanded: !this.state.expanded });
-    };
-
-    handleLeftOpen = () => this.toggleDrawer('left', true);
-    handleLeftClose = () => this.toggleDrawer('left', false);
-
     render() {
         const classes = this.props.classes;
-
-        const mailFolderListItems = (
-            <div>
-                <Card className={classes.card}>
-                    <CardMedia
-                        className={classes.media}
-                        image={process.env.PUBLIC_URL + 'banner/stars.jpg'}
-                        title="Contemplative Reptile"
-                    />
-                </Card>
-                <ListItem button>
-                    <ListItemIcon>
-                        <BusinessIcon />
-                    </ListItemIcon>
-                    <NavLink activeClassName='active' to='hours'>Hours</NavLink>
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <PhoneIcon />
-                    </ListItemIcon>
-                    <NavLink to='call'>Call</NavLink>
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <StarIcon />
-                    </ListItemIcon>
-                    <NavLink to='reviews'>Reviews</NavLink>
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <SendIcon />
-                    </ListItemIcon>
-                    <NavLink to='mail'>Mail</NavLink>
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <DirectionsIcon />
-                    </ListItemIcon>
-                    <NavLink to='directions'>Directions</NavLink>
-                </ListItem>
-            </div>
-        );
-
-        const otherMailFolderListItems = (
-            <div>
-                <ListItem button>
-                    <ListItemIcon>
-                        <DateRangeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Appointments" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <ComputerIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Careers" />
-                </ListItem>
-            </div>
-        );
-
-        const sideList = (
-            <div>
-                <List className={classes.list} disablePadding>
-                    {mailFolderListItems}
-                </List>
-                <Divider />
-                <List className={classes.list} disablePadding>
-                    {otherMailFolderListItems}
-                </List>
-            </div>
-        );
-
-        const fullList = (
-            <div>
-                <List className={classes.listFull} disablePadding>
-                    {mailFolderListItems}
-                </List>
-                <Divider />
-                <List className={classes.listFull} disablePadding>
-                    {otherMailFolderListItems}
-                </List>
-            </div>
-        );
-
         return (
             <BrowserRouter>
             <div>
                 <div className={classes.root}>
-                    <Toolbar>
-                        <IconButton color="contrast" aria-label="Menu"  onClick={this.handleLeftOpen}>
-                            <MenuIcon />
-                        </IconButton>
-                        <Drawer
-                            open={this.state.open.left}
-                            onRequestClose={this.handleLeftClose}
-                            onClick={this.handleLeftClose}>
-                            {sideList}
-                        </Drawer>
-                        <Typography type="title" className={classes.flex}>
-                            Home
-                        </Typography>
-                    </Toolbar>
 
-                    <Route exact path='/' render={() => <h1> Home </h1>} />
+                    <MySideBar>
+                    </MySideBar>
+
+                    <Route exact path='/' render={() => <h1> </h1>} />
+
                     <Route
                         exact
-                        path='/hours'
-                        render={() => <h1> Hours </h1>}
+                        path='/call'
+                        render={() => <h1>  </h1>}
+                    />
+                    <Route
+                        exact
+                        path='/reviews'
+                        render={() => <h1> </h1>}
+                    />
+                    <Route
+                        exact
+                        path='/mail'
+                        render={() => <h1> </h1>}
+                    />
+                    <Route
+                        exact
+                        path='/directions'
+                        render={() => <h1> </h1>}
                     />
                 </div>
                 <div>
---
+++ barbershop/src/components/sidebar.jsx
@@ -0,0 +1,191 @@
+import React, { Component } from 'react';
+import Drawer from 'material-ui/Drawer';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import {BrowserRouter, Route, NavLink } from 'react-router-dom'
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+
+import BusinessIcon from 'material-ui-icons/Business';
+import PhoneIcon from 'material-ui-icons/Phone';
+import DirectionsIcon from 'material-ui-icons/Directions';
+import DateRangeIcon from 'material-ui-icons/DateRange';
+import StarIcon from 'material-ui-icons/Star';
+import SendIcon from 'material-ui-icons/Send';
+import HomeIcon from 'material-ui-icons/Home';
+import ComputerIcon from 'material-ui-icons/Computer';
+import Toolbar from 'material-ui/Toolbar';
+import MenuIcon from 'material-ui-icons/Menu';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+    },
+    media: {
+        height: 300,
+        marginBottom:20,
+    },
+    list: {
+        width: 250,
+        flex: 'initial',
+    },
+    listFull: {
+        width: 'auto',
+        flex: 'initial',
+    },
+    flex: {
+        flex: 1,
+        color: '#FAFAFA'
+    },
+});
+
+class SideBarItem extends Component {
+    state = {
+        open: {
+            top: false,
+            left: false,
+            bottom: false,
+            right: false,
+        },
+        expanded: false
+    };
+    toggleDrawer = (side, open) => {
+        const drawerState = {};
+        drawerState[side] = open;
+        this.setState({ open: drawerState });
+    };
+    handleExpandClick = () => {
+        this.setState({ expanded: !this.state.expanded });
+    };
+
+    handleLeftOpen = () => this.toggleDrawer('left', true);
+    handleLeftClose = () => this.toggleDrawer('left', false);
+
+    render(){
+        const classes = this.props.classes;
+
+        const mailFolderListItems = (
+            <div>
+                <Card className={classes.card}>
+                    <CardMedia
+                        className={classes.media}
+                        image={process.env.PUBLIC_URL + 'banner/stars.jpg'}
+                        title="Contemplative Reptile"
+                    />
+                </Card>
+                <ListItem button>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <div>
+                        <NavLink activeClassName='active' exact to='/' >Home</NavLink>
+                    </div>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <BusinessIcon />
+                    </ListItemIcon>
+                    <NavLink activeClassName='active' to='hours'>Hours</NavLink>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <PhoneIcon />
+                    </ListItemIcon>
+                    <NavLink activeClassName='active' to='call'>Call</NavLink>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <StarIcon />
+                    </ListItemIcon>
+                    <NavLink activeClassName='active' to='reviews'>Reviews</NavLink>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <NavLink activeClassName='active' to='mail'>Mail</NavLink>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DirectionsIcon />
+                    </ListItemIcon>
+                    <NavLink activeClassName='active' to='directions'>Directions</NavLink>
+                </ListItem>
+            </div>
+        );
+
+        const otherMailFolderListItems = (
+            <div>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DateRangeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Appointments" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <ComputerIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Careers" />
+                </ListItem>
+            </div>
+        );
+
+        const sideList = (
+            <div>
+                <List className={classes.list} disablePadding>
+                    {mailFolderListItems}
+                </List>
+                <Divider />
+                <List className={classes.list} disablePadding>
+                    {otherMailFolderListItems}
+                </List>
+            </div>
+        );
+
+        const fullList = (
+            <div>
+                <List className={classes.listFull} disablePadding>
+                    {mailFolderListItems}
+                </List>
+                <Divider />
+                <List className={classes.listFull} disablePadding>
+                    {otherMailFolderListItems}
+                </List>
+            </div>
+        );
+        return (
+            <Toolbar>
+                <IconButton color="contrast" aria-label="Menu"  onClick={this.handleLeftOpen}>
+                    <MenuIcon />
+                </IconButton>
+                    <Drawer
+                        open={this.state.open.left}
+                        onRequestClose={this.handleLeftClose}
+                        onClick={this.handleLeftClose}>
+                        {sideList}
+                        >
+                        <NavLink activeClassName='active' exact to='Hours'>
+                            <MenuItem onTouchTap={this.handleLeftClose}>
+                                dfsd </MenuItem>
+                        </NavLink>
+                    </Drawer>
+                <Typography type="title" className={classes.flex}>
+                    Home
+                </Typography>
+            </Toolbar>
+
+        )
+    }
+}
+
+
+SideBarItem.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SideBarItem);
